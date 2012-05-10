444782b3b5c5040bbf11bace0e8c5e79199efc57
Jonathan Dance <jd@wuputah.com>
2012-05-09 23:54:44 -0700

    add vim

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 VERSION=1.5.21
-PREFIX='/app/mutt'
+PREFIX='/app/local'
 MAILDIR='/app/Mail'
 CONFIGURE="./configure --enable-imap --prefix=$PREFIX --with-mailpath=$MAILDIR"
 
@@ -8,3 +8,9 @@ tar xzf mutt.tar.gz
 rm mutt.tar.gz
 vulcan build -v -n mutt -s mutt-$VERSION -o $PWD/mutt.tgz -p $PREFIX -c "mkdir $MAILDIR && $CONFIGURE && make && make install"
 rm -rf mutt-$VERSION
+
+curl "ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2" >vim.tar.bz2
+tar xjf vim.tar.bz2
+rm vim.tar.bz2
+vulcan build -v -n vim -s vim73 -o $PWD/vim.tgz -p $PREFIX -c "./configure --prefix=$PREFIX && make && make install"
+rm -rf vim73
